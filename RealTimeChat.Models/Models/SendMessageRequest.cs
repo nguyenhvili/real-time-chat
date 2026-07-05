37c850b498285d6f02bfc7fe18a0588b5bf789e1
Hoang Nam Nguyen <namhoangng.nguyenn@gmail.com>
2026-07-05 13:55:24 +0200

    fix: warnings

--- RealTimeChat.Models/Models/SendMessageRequest.cs
+++ RealTimeChat.Models/Models/SendMessageRequest.cs
@@ -7,7 +7,7 @@ public class SendMessageRequest
     public string? Type { get; set; }
 
     [MaxLength(100)]
-    public string? SenderName { get; set; }
+    public required string SenderName { get; set; }
 
-    public string? Text { get; set; }
+    public required string Text { get; set; }
 }
